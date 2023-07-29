e5c3d26a827ce38e847174b05585d785d80a22b7
Tamas <soostamas.hu@gmail.com>
2023-07-29 17:17:01 +0200

    tidy

--- src/InventoryRegistry.sol
+++ src/InventoryRegistry.sol
@@ -12,96 +12,72 @@ pragma solidity ^0.8.13;
 
 import "solidstate-solidity/token/ERC1155/SolidStateERC1155.sol";
 import "solmate/auth/Owned.sol";
-import "openzeppelin-contracts/proxy/utils/Initializable.sol";
+import "openzeppelin-contracts/contracts/proxy/utils/Initializable.sol";
 
 contract InventoryRegistry is SolidStateERC1155, Owned, Initializable {
+    uint256 private _itemDefID = 1;
+    uint256 private _tokenID = 1;
+
+    mapping(uint256 => string) public itemDefIDToURI;
+    mapping(uint256 => uint256[]) public itemDefIDToTokenIDs;
+
+    event ItemDefinitionCreated(
+        uint256 indexed itemDefID,
+        uint256 indexed tokenID,
+        string indexed itemDefURI
+    );
+
+    event ItemDefinitionUpdated(
+        uint256 indexed itemDefID,
+        string indexed oldItemDefURI,
+        string indexed newItemDefURI
+    );
+
     constructor(address _owner) Owned(_owner) SolidStateERC1155() {}
 
     function init(bytes calldata data) external payable initializer {
         owner = abi.decode(data, (address));
     }
 
-    uint256 private tokenId = 1;
-    uint256 private definitionId = 1;
-
-    // mapping(uint256 itemDefinitionId => string definitionUri)
-    //     public definitionIdToUri;
-    // mapping(uint256 itemDefinitionId => uint256[] tokenIds)
-    //     public definitionIdToTokenIds;
-    // mapping(uint256 tokenId => uint256 itemDefinitionId)
-    //     public tokenIdToDefinitionId;
-
-    mapping(uint256 => string) public definitionIdToUri;
-    mapping(uint256 => uint256[]) public definitionIdToTokenIds;
-    mapping(uint256 => uint256) public tokenIdToDefinitionId;
-    mapping(uint256 => string) public tokenIdToAttribute;
-
-    event SetTokenDefinition(
-        uint256 indexed tokenId, uint256 indexed definitionId
-    );
-
-    event CreatedDefinition(
-        uint256 indexed definitionId,
-        uint256 indexed tokenId,
-        string indexed definitionUri
-    );
-
-    event UpdatedDefinition(
-        uint256 indexed definitionId,
-        string indexed oldDefinitionUri,
-        string indexed newDefinitionUri
-    );
-
-    function createItemDefinition(string calldata definitionUri)
+    function createItemDefinition(string calldata itemDefURI)
         external
         onlyOwner
     {
-        definitionIdToUri[definitionId] = definitionUri;
-        definitionIdToTokenIds[definitionId].push(tokenId);
-        emit CreatedDefinition(definitionId, tokenId, definitionUri);
-        definitionId++;
-        tokenId++;
+        itemDefIDToURI[_itemDefID] = itemDefURI;
+        itemDefIDToTokenIDs[_itemDefID].push(_tokenID);
+        emit ItemDefinitionCreated(_itemDefID, _tokenID, itemDefURI);
+        _itemDefID++;
+        _tokenID++;
     }
 
     function updateItemDefinition(
-        uint256 _definitionId,
-        string calldata newDefinitionUri
+        uint256 itemDefID,
+        string calldata newItemDefURI
     ) external onlyOwner {
-        string memory oldDefinitionUri = definitionIdToUri[_definitionId];
-        definitionIdToUri[_definitionId] = newDefinitionUri;
-        emit UpdatedDefinition(tokenId, oldDefinitionUri, newDefinitionUri);
+        string memory oldItemDefURI = itemDefIDToURI[itemDefID];
+        itemDefIDToURI[itemDefID] = newItemDefURI;
+        emit ItemDefinitionUpdated(_tokenID, oldItemDefURI, newItemDefURI);
     }
 
-    function setAttribute(uint256 tokenId_, string calldata attribute_)
-        external
-        onlyOwner
-    {
-        // onlyAttributor(tokenIdToDefinitionId[tokenId]) {
-        tokenIdToAttribute[tokenId_] = attribute_;
-    }
-
-    function burnFrom(address player, uint256 tokenId_, uint256 amount)
-        public
-    {
+    function burnFrom(address player, uint256 tokenID, uint256 amount) public {
         require(isApprovedForAll(player, msg.sender), "Caller is not approved");
-        _burn(player, tokenId_, amount);
+        _burn(player, tokenID, amount);
     }
 
     function mintTo(
         address to,
         uint256 amount,
-        uint256 itemDefinitionId,
+        uint256 itemDefID,
         bool isFungible
-    ) public onlyOwner returns (uint256 mintedTokenId) {
+    ) public onlyOwner returns (uint256 mintedTokenID) {
         if (isFungible) {
-            uint256 existingTokenId =
-                definitionIdToTokenIds[itemDefinitionId][0];
-            _mint(to, existingTokenId, amount, "");
-            return existingTokenId;
+            uint256 existingTokenID = itemDefIDToTokenIDs[itemDefID][0];
+            _mint(to, existingTokenID, amount, "");
+            return existingTokenID;
         } else {
-            _mint(to, tokenId, amount, "");
-            tokenId++;
-            return tokenId - 1;
+            _mint(to, _tokenID, amount, "");
+            _tokenID++;
+            return _tokenID - 1;
         }
     }
 }
