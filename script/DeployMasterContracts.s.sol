05f95323bb854c74f6a7be8d01bfa7cbf5a10aa6
Tamas <soostamas.hu@gmail.com>
2023-08-13 17:56:14 +0200

    Chore: Add Deploy Master Contracts Script (#4)

---
+++ script/DeployMasterContracts.s.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.15;
+
+import "forge-std/Script.sol";
+import "BoringSolidity/BoringFactory.sol";
+import "src/InventoryController.sol";
+import "src/InventoryRegistry.sol";
+import "src/behaviors/PurchaseItemEth.sol";
+
+contract DeployMasterContracts is Script {
+    function run()
+        public
+        returns (
+            BoringFactory factory,
+            InventoryRegistry registry,
+            InventoryController controller,
+            PurchaseItemEth purchase
+        )
+    {
+        vm.startBroadcast();
+
+        // factory
+        factory = new BoringFactory();
+
+        // inventory
+        registry = new InventoryRegistry(tx.origin);
+        controller = new InventoryController(tx.origin);
+
+        // behaviors
+        purchase =
+        new PurchaseItemEth(tx.origin, address(controller), IInventoryRegistry(address(registry)));
+
+        vm.stopBroadcast();
+    }
+}
